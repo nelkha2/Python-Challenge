1f9b60b58fa3e674c8d025cf0ed7bf72a83279cb
Nader Elkhatib <Nader.Elkhatib@me.com>
2019-06-17 23:43:31 -0500

    Progress - Remaining: Export to Text File

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,93 @@
 #PyBank
+#Goal: ouput metrics via print screen and export to text file 
+
+#Reading CSV file: raw data
+import os
+import csv
+
+# Variables for storing data from csv file and running loops  
+Dates = []
+i = 0
+Profit_Losses = []
+Profit_Losses_Changes = []
+j = 1
+
+csvpath = os.path.join("budget_data.csv")
+with open(csvpath, newline = "") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter =",")
+    csv_header = next(csvreader)
+
+    # month count & net profit/loss calculation
+    for row in csvreader:
+        Dates.append(row[0])
+        i = i + 1
+        Profit_Losses.append(int(row[1]))
+        total = sum(Profit_Losses,0)
+    
+    # monthly changes (first element of Monthly Change array is zero since it cannot be calculated for the first month of the list)
+    Profit_Losses_Changes.append(int(0))
+    for j in range(1,i):
+        change = Profit_Losses[j] - Profit_Losses[j-1]
+        Profit_Losses_Changes.append(int(change))
+        j = j + 1
+    
+    # Monthly changes average
+    Total_Changes = sum(Profit_Losses_Changes,0)
+    Changes_Count = len(Profit_Losses_Changes)
+    Monthly_Ave =round(Total_Changes / Changes_Count,2) 
+
+    #Largest Increase & Decrease Monthly Changes 
+    Max_Monthly_Change = int(max(Profit_Losses_Changes))
+    Min_Monthly_Change = int(min(Profit_Losses_Changes))
+    Monthly_Changes_Combo = dict (zip(Profit_Losses_Changes,Dates))
+    
+    #Print Report
+    print("Financial Analysis")
+    print("----------------------------------------------------")
+    print(f'Total Months: {i}')
+    print(f'Net Profit/Loss: ${total}')
+    print(f'Average Monthly Change: ${Monthly_Ave}')
+    print(f'Greatest Increase in Profits: {Monthly_Changes_Combo[int(Max_Monthly_Change)]} (${Max_Monthly_Change})')
+    print(f'Greatest Decrease in Profits: {Monthly_Changes_Combo[int(Min_Monthly_Change)]} (${Min_Monthly_Change})')
+    print(f'----------------------------------------------------')
+
+    
+
+
+
+   
+
+
+
+
+
+    
+    
+
+
+    
+
+
+
+  
+
+        
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,77 @@
 #PyPoll
+#Goal: print voting metrics and export to text file
+
+import os
+import csv
+
+# Variables and arrays
+i = 0
+Candidates = []
+Candidates_List = []
+w = 0
+z = 0
+l = 0
+o = 0
+
+#Reading CSV file / Raw Data
+csvpath = os.path.join("election_data.csv")
+with open(csvpath, newline="") as csvfile: 
+    csvreader = csv.reader(csvfile,delimiter = ",")
+    csv_header = next(csvreader)
+    
+    #Total Votes calculation & extracting candidate names
+    for row in csvreader:
+        Candidates.append(row[2])
+        Candidates_List.append(row[2])
+        i = i + 1
+    
+    #List of unique candidates 
+    Candidates_List =list(dict.fromkeys(Candidates))
+
+    #Khan's total votes & win percentage
+    for n in range(int(len(Candidates))):
+        if Candidates[n-1] == Candidates_List[0]:
+            w = w +1
+    Khan_win_perc = round(float((w/len(Candidates)) * 100),3)
+
+    #Correy's total votes & win percentage
+    for n in range(int(len(Candidates))):
+        if Candidates[n-1] == Candidates_List[1]:
+            z = z + 1
+    Correy_win_perc = round(float((z/len(Candidates)) * 100),3)
+
+    #Li's total votes & win percentage
+    for n in range(int(len(Candidates))):
+        if Candidates[n-1] == Candidates_List[2]:
+            l = l + 1
+    Li_win_perc = round(float((l/len(Candidates)) * 100),3)
+    
+
+    #O'Tooley's total votes & win percentage
+    for n in range(int(len(Candidates))):
+        if Candidates[n-1] == Candidates_List[3]:
+            o = o + 1
+    OTooley_win_perc = round(float((o/len(Candidates)) * 100),3)
+
+    #Winner 
+    Perc_List = [Khan_win_perc,Correy_win_perc,Li_win_perc,OTooley_win_perc]
+    Per_Name_Combo = dict (zip(Perc_List,Candidates_List))
+    Winner = float (max(Perc_List))
+
+    #Print Report 
+    print(f'Electronic Results')
+    print(f'---------------------------------------------------')
+    print(f'Total Votes: {int(i)}')
+    print(f'---------------------------------------------------')
+    print(f'{Candidates_List[0]}: {Khan_win_perc}% ({w})')
+    print(f'{Candidates_List[1]}: {Correy_win_perc}% ({z})')
+    print(f'{Candidates_List[2]}: {Li_win_perc}% ({l})')
+    print(f'{Candidates_List[3]}: {OTooley_win_perc}% ({o})')
+    print(f'...................................................')
+    print(f'Winner: {Per_Name_Combo[Winner]}')
+    print(f'---------------------------------------------------')
+
+
+
+   
+
